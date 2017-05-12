48c0bf64c12afd8f7fc1263ff4d12c203d1899c5
Sam Carlton <sam@sam.lc>
2017-05-12 11:04:41 -0500

    Add comments for acceptable page arguments

--- config-sample.php
+++ config-sample.php
@@ -4,6 +4,15 @@
 	define("FB_APP_ID", "replace_me");
 	define("FB_APP_SECRET", "replace_me");
 
+
+	// Formats that work
+
+	// https://www.facebook.com/KIRO7Seattle/
+	// https://www.facebook.com/112779115408517
+	// KIRO7Seattle
+	// 112779115408517
+
+
 	// Live Streams for testing
 	// https://www.facebook.com/livemap/
 	define("FB_PAGE", "replace_me");
