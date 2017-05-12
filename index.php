76caffdd7dd58b09fd66651c8c183058fd37eb60
Sam Carlton <sam@sam.lc>
2017-05-12 14:37:47 -0500

    Set field to populate with stream from query

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ $FacebookLive = new GetFacebookLiveStream([
 
       <div class="field inline-block is-grouped column is-6 is-offset-3">
         <p class="control is-expanded">
-          <input class="input inline is-large" type="text" name="stream" placeholder="Paste an FB page url">
+          <input class="input inline is-large" type="text" name="stream" placeholder="Paste an FB page url" value="<?= @$_GET['stream'] ?>">
         </p>
         <p class="control">
           <input class="button inline is-large" type="submit" value="Submit">
