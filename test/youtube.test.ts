d721bf1cae899964f8a54c081208d83ea30cb3eb
Sam Carlton <sam@sam.lc>
2022-09-18 10:28:33 -0500

    Test YouTubeCheckLive

--- test/youtube.test.ts
+++ test/youtube.test.ts
@@ -1,8 +1,12 @@
 import { assert, expect, test } from 'vitest'
 
+import { YouTubeCheckLive } from '../lib/youtube'
+
+
+
 // Edit an assertion and save to see HMR in action
 
-test('Can test', () => {
+test( 'Can test', () => {
 
     try { 
         assert(true)
@@ -10,4 +14,73 @@ test('Can test', () => {
         expect(e).toBeUndefined()
     }
 
-})
+} )
+
+const liveChannels = new Map([
+
+    // Not Live
+    [ 'JordanHJ', 'UCenBYEupFr45ztQdv0Q00Gg' ],
+    [ 'HowCommunicationWorks', 'UCQ0v32jT6Uf3FQ5TI4tudMg' ],
+    [ 'jawed', 'UC4QobU6STFB0P71PMvOGN5A' ], 
+    [ 'Random', 'UC2ARRjsp3TdvVZvj5R9Mhpg' ], 
+
+
+    // Live
+    [ 'Random Live', 'UCyq7r6SK3M-neYZ28iD_Bgg' ], 
+    [ 'KPRC Live', 'UCKQECjul8nw1KW_JzfBTP1A' ], 
+    [ 'SkyNews Live', 'UCoMdktPbSTixAyNGwb-UYkQ' ], 
+    [ 'ABC Live', 'UCBi2mrWuNuyYy4gbM6fU18Q' ], 
+    // [ 'WAPO Mobile Live Videos List', 'UCHd62-u_v4DvJ8TCFtpi4GA' ], 
+
+
+
+    // [ 'KPRC', 'KPRC2Click2Houston' ], 
+    // [ 'KPRC', 'kprc2' ], 
+    // [ 'SkyNews Mobile Live Videos List', 'SkyNews' ], 
+    // [ 'ABC Mobile Live Videos List', 'ABCNews' ], 
+
+    // [ 'WAPO Mobile Live Videos List', 'WashingtonPost' ],
+
+    // [ 'JordanHJ', 'JordanHJ' ],
+    // [ 'HowCommunicationWorks', 'HowCommunicationWorks' ],
+])
+
+function normalResponseUrl ( url: string, wordsToReplace: Array<string> ) {
+
+    // Make a relative url
+    const origin = new URL( url ).origin
+
+    const relativeUrl = url.replace( origin, '' )
+
+    return wordsToReplace.reduce( ( relativeUrl: string, word: string ) => {
+        return relativeUrl.replace( word, '' )
+    }, relativeUrl )
+    
+}
+
+test( 'Can detect live channels', async () => {
+
+    const liveCheck = new YouTubeCheckLive({
+        identifiers: liveChannels
+    })
+
+    // Run check
+    const liveResults = await liveCheck.check()
+    
+    let previousResponseHeaders:any = null
+
+    // Compare response headers for live channels
+    for ( const responseParts of liveResults ) {
+
+        if ( previousResponseHeaders !== null ) {
+            expect( responseParts ).toEqual( previousResponseHeaders )
+        }
+
+        previousResponseHeaders = responseParts
+
+    }
+
+
+
+} )
+
