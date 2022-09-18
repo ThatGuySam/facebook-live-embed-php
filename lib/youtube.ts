7ba50d9325713d32941e672e214cf5640861f96d
Sam Carlton <sam@sam.lc>
2022-09-18 10:23:28 -0500

    Add parseResponseParts helper

--- lib/youtube.ts
+++ lib/youtube.ts
@@ -1,5 +1,5 @@
 import https from 'https'
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { registerInterceptor } from 'axios-cached-dns-resolve'
 import httpTimer from '@szmarczak/http-timer'
 
@@ -74,4 +74,51 @@ function parseCookieValues ( value: Array<string> ) {
 
         return mappedCookieParts
     } )
+}
+
+export function parseResponseParts ( response: AxiosResponse, identifier: string = 'no identifier' ) {
+
+    const headerEntries = Object.entries( response.headers )
+
+    const responseHeaders = headerEntries.map( ( [ key, initialValue ]:any, entryIndex ) => {
+
+        const index = liveIgnoredIndexes.has( key ) ? 'ignored' : entryIndex
+        const value = liveIgnoredValues.has( key ) ? 'ignored' : initialValue
+
+        // If the header value is set-cookie then map it into an array
+        // so that we can compare them and omit data only specfic to this request
+        if ( key === 'set-cookie' ) {
+            
+            const parsedCookies = parseCookieValues( value )
+
+            return [ key, { index, value: parsedCookies } ]
+        }
+
+
+        // If the header is content-length then round it to the nearest 100
+        if ( key === 'content-length' ) {
+
+            // const nearest = 100_000
+            // const roundedValue = Math.round( Number( value ) / nearest )// * nearest
+
+            return [ key, { index, value: 'TODO' } ]
+        }
+
+        if ( key === 'location' ) {
+            return [ key, { index, value: 'TODO' } ]
+        }
+
+        return [ key, { index, value } ]
+    } )
+
+    // console.log( 'fetchedUrls', response.request.res.responseUrl )
+    // console.log( 'count', response.request._redirectable._redirects )
+    
+    return {
+        headers: Object.fromEntries( responseHeaders ),
+        // redirectCount: response.request._redirectable._redirectCount,
+        // redirects: response.request._redirectable._redirects,
+        // responseUrl: normalResponseUrl( response.request.res.responseUrl, [ identifier ] ),
+        // fetchedUrls: response.request.res.fetchedUrls,
+    }
 }
