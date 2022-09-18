812cbf0ff69decb8b81ac3b699ca5deeeb2a65e2
Sam Carlton <sam@sam.lc>
2022-09-18 09:34:54 -0500

    Add youtubeAxios instance

--- lib/youtube.ts
+++ lib/youtube.ts
@@ -1,4 +1,26 @@
+import https from 'https'
+import axios from 'axios'
+import { registerInterceptor } from 'axios-cached-dns-resolve'
+import httpTimer from '@szmarczak/http-timer'
 
+const youTubeAxiosConfig:any = {
+    // transport: {
+    //     // request: function httpsWithTimer(...args) {
+    //     //     const request = https.request.apply(null, args)
+    //     //     httpTimer(request)
+    //     //     return request
+    //     // },
+    //     // response: function httpsWithTimer(...args) {
+    //     //     const response = https.request.apply(null, args)
+    //     //     httpTimer(response)
+    //     //     return response
+    //     // }
+    // }
+}
+
+export const youTubeAxios = axios.create( youTubeAxiosConfig )
+
+registerInterceptor(youTubeAxios)
 
 export function makeYouTubeUrl ( parts:any ) {
     // return `https://m.youtube.com/channel/${ youtubeId }/live`
