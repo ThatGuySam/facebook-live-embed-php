4a59261ba94cf33a9d4cb7ebe763126a216d4d6c
Sam Carlton <sam@sam.lc>
2022-09-18 10:21:24 -0500

    Add parseCookieValues helper

--- lib/youtube.ts
+++ lib/youtube.ts
@@ -20,7 +20,27 @@ const youTubeAxiosConfig:any = {
 
 export const youTubeAxios = axios.create( youTubeAxiosConfig )
 
+// Setup the interceptor
+// so that DNS for our subsequent requests will be cached
 registerInterceptor(youTubeAxios)
+
+
+const liveIgnoredValues = new Set([
+    'VISITOR_INFO1_LIVE',
+    'YSC',
+    'Expires',
+    'date'
+])
+
+
+// Keys for data that varies between livestream headers
+const liveIgnoredIndexes = new Set([
+    'report-to',
+    'cross-origin-opener-policy-report-only',
+    'permissions-policy',
+    'x-frame-options',
+    'strict-transport-security'
+])
 
 export function makeYouTubeUrl ( parts:any ) {
     // return `https://m.youtube.com/channel/${ youtubeId }/live`
@@ -34,4 +54,24 @@ export function makeYouTubeUrl ( parts:any ) {
     }
 
     return url.toString()
+}
+
+function parseCookieValues ( value: Array<string> ) {
+    return value.map( ( cookie: string ) => {
+        // Split by semicolons
+        const cookieParts = cookie.split( ';' )
+
+        // Map the parts into an array of key value pairs
+        const mappedCookieParts = cookieParts.map( ( part: string ) => {
+            const [ key, value ] = part.split( '=' ).map( ( part: string ) => part.trim() )
+
+            if ( liveIgnoredValues.has( key ) ) {
+                return [ key, 'ignored' ]
+            }
+
+            return [ key, value ]
+        } )
+
+        return mappedCookieParts
+    } )
 }
